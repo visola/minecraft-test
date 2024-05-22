580c635302010289ef437b76bfdc3445c6a6d304
Vinicius Isola <viniciusisola@gmail.com>
2024-05-21 20:24:10 -0400

    Zip correctly, zip all into .mcproject file

--- build.sh
+++ build.sh
@@ -3,7 +3,11 @@
 rm -Rf dist/
 mkdir dist/
 
-zip -rj dist/behavior_pack.mcpack src/behavior_pack/*
-zip -rj dist/resource_pack.mcpack src/resource_pack/*
+OUTPUT_DIR=$(pwd)/dist
+echo "Output dir is $OUTPUT_DIR"
+
+(cd src/behavior_pack && zip -r $OUTPUT_DIR/behavior_pack.mcpack ./*)
+(cd src/resource_pack && zip -r $OUTPUT_DIR/resource_pack.mcpack ./*)
+(cd $OUTPUT_DIR && zip my_pack.mcproject behavior_pack.mcpack resource_pack.mcpack)
 
 cp src/index.html dist/
